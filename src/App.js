d2e1399576f79f53436c82b3f9f26b9fa6977c3d
Kristina Taing <kataing12@gmail.com>
2020-08-02 21:31:49 -0700

    implemented register, login, & homepage routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+// Components
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Form from './pages/Form';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Form} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
