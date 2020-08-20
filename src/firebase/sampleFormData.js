a3b89e00609d0f6ff99b852cba026402939c750f
Kristina Taing <kataing12@gmail.com>
2020-08-20 15:34:07 -0700

    created sample lms form data

---
+++ src/firebase/sampleFormData.js
@@ -0,0 +1,19 @@
+const sampleFormData = {
+  // The key represents the Discovery Data id
+  2: {
+    // The name represents the Discovery Data label
+    name: 'Overall Utilization',
+    // Tier Specification: 1 - Required, 2 - Want to Have, 0 - Optional
+    value: 2,
+  },
+  3: {
+    name: 'calculates students time online',
+    value: 1,
+  },
+  4: {
+    name: "notify's students progress",
+    value: 0,
+  },
+};
+
+export default sampleFormData;
