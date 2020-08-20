e69cb5a62c2e87f9b964ba4b90f8a503efc2efbb
Kristina Taing <kataing12@gmail.com>
2020-08-20 15:34:30 -0700

    updated seeding script

--- src/firebase/seed.js
+++ src/firebase/seed.js
@@ -1,12 +1,19 @@
 import { firestore } from './firebase.utils';
 import DISCOVERY_DATA from './discoverydata';
+import LMS_DATA from './lmsdata';
 
-const data = DISCOVERY_DATA;
-
-const seed = async () => {
+const seedDiscoveryData = async () => {
+  const data = DISCOVERY_DATA;
   for (let i = 0; i < data.length; i++) {
     await firestore.collection('discovery').doc(data[i].id).set(data[i]);
   }
 };
 
-export default seed;
+const seedLmsData = async () => {
+  const data = LMS_DATA;
+  for (let i = 0; i < data.length; i++) {
+    await firestore.collection('lms').doc(data[i].id).set(data[i]);
+  }
+};
+
+export { seedDiscoveryData, seedLmsData };
